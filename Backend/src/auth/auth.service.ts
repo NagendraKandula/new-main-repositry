3ee63b0a47cd40018b996afbd027c4e55d181e73
NagendraKandula <139203478+NagendraKandula@users.noreply.github.com>
2025-10-01 01:38:10 +0530

    Update auth.service.ts

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -53,7 +53,7 @@ export class AuthService {
     res.cookie('access_token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'lax',
+      sameSite: 'none',
       expires: new Date(Date.now() + 1 * 60 * 60 * 1000),
     });
 
